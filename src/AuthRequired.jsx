e201946fe3ae7a42dc64a4f03f4da0f46c0bde83
Danish <jdanish78635@gmail.com>
2024-02-15 00:26:13 +0530

    404 page and login protection partial

--- src/AuthRequired.jsx
+++ src/AuthRequired.jsx
@@ -2,7 +2,9 @@ import React from "react"
 import { Outlet, Navigate } from "react-router-dom"
 
 const AuthRequired = () => {
-    const isLoggedIn = true
+    const isLoggedIn = false
     return isLoggedIn ? <Outlet/> : <Navigate to = "/login"/>
 }
  export default AuthRequired
+
+
